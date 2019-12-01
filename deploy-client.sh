16e30525aac40e21fb8487dff4c3aff1af9af6bf
Shivam Goyal <shivamgoyal1506@outlook.com>
2019-12-01 13:35:37 +0530

    updated deploy scripts

--- deploy-client.sh
+++ deploy-client.sh
@@ -6,21 +6,25 @@ GIT_TOKEN="8kcDJeNjwjxBHztdq1tx"
 GIT_BRANCH="production"
 GIT_CLONE_DIR="temp_client"
 
-TARGET_DIR="/home/shivam/fffffff"
+TARGET_DIR="/var/www/wtd_client"
 # ------------------------------------------------------------>
 
 # Clean and Clone
+echo ">>>> Cleaning previous build!"
 rm -rf ${GIT_CLONE_DIR}
-git clone --branch=${GIT_BRANCH} https://gitlab-ci-token:${GIT_TOKEN}@gitlab.com/${GIT_SLUG}.git ${GIT_CLONE_DIR}
+echo ">>>> Cloning the latest build!"
+git clone --branch=${GIT_BRANCH} --depth=1 https://gitlab-ci-token:${GIT_TOKEN}@gitlab.com/${GIT_SLUG}.git ${GIT_CLONE_DIR}
 if [ $? != 0 ]; then exit $?; fi
 cd ${GIT_CLONE_DIR}
 
 # Sync with the latest
-rsync -r ./* ${TARGET_DIR}/ --except .git
+echo ">>>> Synchronizing with the latest version!"
+sudo rsync -r ./* ${TARGET_DIR}/ --exclude=.git
 
 # After syncing
-sudo systemctl restart wtd_client.service
+echo ">>>> Reloading nginx!"
+sudo service nginx reload
 if [ $? != 0 ]; then exit $?; fi
 # ------------------------------------------------------------>
-echo "Succussfully deployed!"
+echo ">>>> Succussfully deployed!"
 exit 0
